199bcce7d7a75441f259a4a00c4f60aa8cf42aa3
adjinurdiman <adjinurdiman@gmail.com>
2020-03-07 23:44:18 +0700

    gabut ajik

--- intromarket2.php
+++ intromarket2.php
@@ -10,11 +10,10 @@ $foto = $data_user['profile'];
 <html lang="en">
 
 <head>
-    <link rel="shortcut icon" href="logo.jpg">
-    <meta charset="UTF-8">
+        <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Welcome! || MARKET</title>
+    <title>M4RKET</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
@@ -25,6 +24,8 @@ $foto = $data_user['profile'];
     </script>
 
     <link rel="stylesheet" href="introMarket2.css">
+    <link rel="shortcut icon" href="favicon.ico">
+
 </head>
 
 <body>
